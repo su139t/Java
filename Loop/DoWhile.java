7f6ab04389be61530bb8e9327e9233111e55015a
su139t <su139t@gmail.com>
2024-12-13 02:33:18 +0530

    Create dowhile loop file

---
+++ Loop/DoWhile.java
@@ -0,0 +1,11 @@
+package Loop;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.out.println("Hello Sumit");
+            i++;
+        }while(i<=10);
+    }
+}
