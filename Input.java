15f04661fe0fcdfe65dd9da805844d9df96f228c
Sumit Birla <155639309+su139t@users.noreply.github.com>
2024-12-14 14:21:29 +0530

    some changes for checking in new system

--- Input.java
+++ Input.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Input {
     public static void main(String[] args) {
+        ///hyy sumit
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         System.out.println(a);
