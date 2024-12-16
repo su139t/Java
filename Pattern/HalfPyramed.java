e76481d2b290244c47960235413cd0de29adf300
su139t <su139t@gmail.com>
2024-12-16 11:34:24 +0530

    Learning Half Pyramid in patterns chapter

---
+++ Pattern/HalfPyramed.java
@@ -0,0 +1,12 @@
+package Pattern;
+
+public class HalfPyramed {
+    public static void main(String[] args) {
+      for(int line = 1 ; line <= 4 ; line++){
+        for(int number = 1 ; number <= line ; number++){
+            System.out.print(number);
+        }
+        System.out.println();
+      }        
+    }
+}
