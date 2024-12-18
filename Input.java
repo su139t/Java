d428a3574c99e36b38ff78c8ded41ed478441ea1
su139t <su139t@gmail.com>
2024-12-18 19:41:31 +0530

    testing banch

--- Input.java
+++ Input.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Input {
     public static void main(String[] args) {
-        ///hyy sumit
+        ///hyy sumit .
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         System.out.println(a);
